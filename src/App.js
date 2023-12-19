f80ca73aee08846ee4510d326e258cfe85253f4e
Anghel Andrei <andrei.anghel2810@gmail.com>
2023-12-19 03:13:40 +0200

    Add communication between Frontend and Backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/advertisement/get/false');
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="posts-container">
+        {data.map((post) => {
+          return (
+              <div className="post-card" key={post.id}>
+                <div className="button">
+                  <div className="delete-btn">${post.id}</div>
+                </div>
+              </div>
+          );
+        })}
+      </div>
   );
 }
 
